63eab6fb0c7e907551446af136726f7ad3880e75
RenzHoly <renzholy@gmail.com>
2017-04-30 23:30:58 +0800

    fix webwxinit & refactor

---
+++ src/api.ts
@@ -0,0 +1,100 @@
+import * as request from 'request-promise-native'
+
+const rq = request.defaults({
+  pool: false,
+  jar: request.jar(),
+  gzip: true,
+})
+
+export async function jslogin(): Promise<{
+  code: number,
+  uuid: string,
+}> {
+  const html = await rq({
+    url: 'https://login.web.wechat.com/jslogin',
+    qs: {
+      appid: 'wx782c26e4c19acffb',
+      redirect_uri: 'https://web.wechat.com/cgi-bin/mmwebwx-bin/webwxnewloginpage',
+      fun: 'new',
+      lang: 'zh_CN',
+      _: Date.now(),
+    }
+  })
+  return {
+    code: parseInt(html.match(/QRLogin.code ?= ?(\d+)/)[1]),
+    uuid: html.match(/QRLogin.uuid ?= ?"(.+)"/)[1],
+  }
+}
+
+export async function login(uuid: string): Promise<{
+  redirect_uri: string,
+}> {
+  let code = 0
+  while (true) {
+    const html = await rq({
+      url: 'https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login',
+      qs: {
+        loginicon: true,
+        uuid,
+        tip: code === 201 ? 0 : 1,
+        r: ~Date.now(),
+        _: Date.now(),
+      },
+    })
+    code = parseInt(html.match(/window.code ?= ?(\d+)/)[1])
+    if (code === 200) {
+      return {
+        redirect_uri: html.match(/redirect_uri="(.+)"/)[1],
+      }
+    }
+  }
+}
+
+export async function webwxnewloginpage(redirect_uri: string): Promise<{
+  wxuin: string,
+  skey: string,
+  wxsid: string,
+  pass_ticket: string,
+}> {
+  const html = await rq({
+    url: redirect_uri,
+    headers: {
+      Host: 'wx.qq.com',
+    },
+    followRedirect: false,
+    simple: false,
+  })
+  return {
+    wxuin: html.match(/<wxuin>(.+)<\/wxuin>/)[1],
+    skey: html.match(/<skey>(.+)<\/skey>/)[1],
+    wxsid: html.match(/<wxsid>(.+)<\/wxsid>/)[1],
+    pass_ticket: html.match(/<pass_ticket>(.+)<\/pass_ticket>/)[1],
+  }
+}
+
+export async function webwxinit(DeviceID: string, Sid: string, Uin: string): Promise<any> {
+  const json = await rq({
+    method: 'POST',
+    url: 'https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxinit',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      Origin: 'https://wx.qq.com',
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.81 Safari/537.36',
+      'Content-Type': 'application/json;charset=UTF-8',
+      Referer: 'https://wx.qq.com/',
+      'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,ja;q=0.2',
+    },
+    qs: {
+      r: ~Date.now(),
+    },
+    json: {
+      BaseRequest: {
+        DeviceID,
+        Sid,
+        Skey: '',
+        Uin,
+      },
+    },
+  })
+  return json
+}
--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,16 @@
-import * as request from 'request-promise-native'
+import 'source-map-support/register'
 import * as qrcode from 'qrcode-terminal'
 import * as https from 'https'
-
-const rq = request.defaults({
-})
+import {
+  jslogin,
+  login,
+  webwxinit,
+  webwxnewloginpage,
+} from './api'
 
 console.debug = console.log
 
-async function jslogin(): Promise<{
-  code: number,
-  uuid: string,
-}> {
-  console.log('uuid')
-  const html = await rq({
-    url: 'https://login.web.wechat.com/jslogin',
-    qs: {
-      appid: 'wx782c26e4c19acffb',
-      redirect_uri: 'https://web.wechat.com/cgi-bin/mmwebwx-bin/webwxnewloginpage',
-      fun: 'new',
-      lang: 'zh_CN',
-      _: Date.now(),
-    }
-  })
-  console.debug(html)
-  return {
-    code: parseInt(html.match(/QRLogin.code ?= ?(\d+)/)[1]),
-    uuid: html.match(/QRLogin.uuid ?= ?"(.+)"/)[1],
-  }
-}
-
-async function login(uuid: string): Promise<{
-  redirect_uri: string,
-}> {
-  console.log('login', uuid)
-  let code = 0
-  while (true) {
-    const html = await request({
-      url: 'https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login',
-      qs: {
-        loginicon: true,
-        uuid,
-        tip: code === 201 ? 0 : 1,
-        r: ~Date.now(),
-        _: Date.now(),
-      },
-      pool: false,
-    })
-    console.debug(html)
-    code = parseInt(html.match(/window.code ?= ?(\d+)/)[1])
-    if (code === 200) {
-      return {
-        redirect_uri: html.match(/redirect_uri="(.+)"/)[1],
-      }
-    }
-  }
-}
-
-async function webwxnewloginpage(redirect_uri: string): Promise<{
-  wxuin: string,
-  skey: string,
-  wxsid: string,
-  pass_ticket: string,
-}> {
-  console.log('webwxnewloginpage', redirect_uri)
-  const html = await request({
-    url: redirect_uri,
-    headers: {
-      Host: 'wx2.qq.com',
-    },
-    followRedirect: false,
-    simple: false,
-    pool: false,
-  })
-  console.debug(html)
-  return {
-    wxuin: html.match(/<wxuin>(.+)<\/wxuin>/)[1],
-    skey: html.match(/<skey>(.+)<\/skey>/)[1],
-    wxsid: html.match(/<wxsid>(.+)<\/wxsid>/)[1],
-    pass_ticket: html.match(/<pass_ticket>(.+)<\/pass_ticket>/)[1],
-  }
-}
+const DeviceId = "e" + ("" + Math.random().toFixed(15)).substring(2, 17)
 
 async function run() {
   const info = await jslogin()
@@ -89,12 +20,12 @@ async function run() {
   }
   const loginUrl = `https://login.weixin.qq.com/l/${info.uuid}`
   qrcode.generate(loginUrl)
-
   const info2 = await login(info.uuid)
   console.log(info2)
-
   const info3 = await webwxnewloginpage(info2.redirect_uri)
   console.log(info3)
+  const info4 = await webwxinit(DeviceId, info3.wxsid, info3.wxuin)
+  console.log(info4)
 }
 
-run()
+run().catch(console.error)
