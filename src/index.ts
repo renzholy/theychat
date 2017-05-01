bbf4bc32fb290b609428a0e9fcbd0cd045ce32e2
RenzHoly <renzholy@gmail.com>
2017-05-01 15:55:20 +0800

    add blessed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'source-map-support/register'
-import * as qrcode from 'qrcode-terminal'
 import * as https from 'https'
-import { sleep } from './utils'
+import * as blessed from 'blessed'
+import { sleep, qrcode } from './utils'
 import {
   jslogin,
   login,
@@ -13,14 +13,52 @@ console.debug = console.log
 
 const DeviceId = "e" + ("" + Math.random().toFixed(15)).substring(2, 17)
 
+const screen = blessed.screen({
+  smartCSR: true,
+})
+screen.render()
+screen.key(['escape', 'q', 'C-c'], function (ch, key) {
+  return process.exit(0);
+})
+
 async function run() {
   const info = await jslogin()
   if (info.code !== 200) {
     console.error('get qrcode error', info)
     return
   }
-  const loginUrl = `https://login.weixin.qq.com/l/${info.uuid}`
-  qrcode.generate(loginUrl)
+
+  const welcome1 = blessed.bigtext({
+    left: 0,
+    content: 'We',
+    style: {
+      fg: 'green',
+    },
+  })
+  const welcome2 = blessed.bigtext({
+    left: 0,
+    content: '  Chat',
+    style: {
+      fg: 'white',
+    },
+  })
+  screen.append(welcome2)
+  screen.append(welcome1)
+
+  const code = blessed.text({
+    bottom: 0,
+    left: 0,
+    width: 'shrink',
+    height: 'shrink',
+    content: await qrcode(`https://login.weixin.qq.com/l/${info.uuid}`, true),
+    tags: true,
+    style: {
+      fg: 'white',
+    },
+  })
+  screen.append(code)
+  screen.render()
+
   const info2 = await login(info.uuid)
   console.log(info2)
   const info3 = await webwxnewloginpage(info2.redirect_uri)
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,15 @@
+import { generate } from 'qrcode-terminal'
+
 export async function sleep(milliseconds: number): Promise<void> {
   return new Promise<void>((resolve, reject) => {
     setTimeout(resolve, milliseconds)
+  })
+}
+
+export async function qrcode(str: string, small: boolean = false): Promise<string> {
+  return new Promise<string>((resolve) => {
+    generate(str, { small }, (code) => {
+      resolve(small ? code.trim() : code)
+    })
   })
 }
