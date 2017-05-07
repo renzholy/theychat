eb52fb50493da80dd9092c926af4d10ebba4205b
RenzHoly <renzholy@gmail.com>
2017-05-08 02:02:04 +0800

    improve format text

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,7 @@
 import { generate } from 'qrcode-terminal'
+import { ucs2 } from 'punycode'
+import { chunk } from 'lodash'
+import * as emojis from 'emojis-list'
 
 export async function sleep(milliseconds: number): Promise<void> {
   return new Promise<void>((resolve, reject) => {
@@ -14,8 +17,27 @@ export async function qrcode(str: string, small: boolean = false): Promise<strin
   })
 }
 
+const LRO = new RegExp(String.fromCharCode(parseInt('0x202D', 16)), 'g') // Left-to-Right Override
+const RLO = new RegExp(String.fromCharCode(parseInt('0x202E', 16)), 'g') // Right-to-Left Override
+const EOF = new RegExp(String.fromCharCode(parseInt('0xFE0F', 16)), 'g')
+
 export function formatText(str: string): string {
-  return str.replace(/<\/?[^>]+>/g, '')
+  let text = str.replace(/<\/?[^>]+>/g, (a) => {
+    if (/emoji emoji(\w+)/.test(a)) {
+      try {
+        return ucs2.encode(chunk(a.match(/emoji(\w+)/)[1].split(''), 5).map(c => parseInt(c.join(''), 16)))
+      } catch (err) {
+        return ''
+      }
+    }
+    return ''
+  })
+  emojis.forEach(emoji => {
+    if (text.indexOf(emoji) >= 0) {
+      text = text.replace(new RegExp(emoji, 'g'), c => c + ' ')
+    }
+  })
+  return text.replace(LRO, '').replace(RLO, '').replace(EOF, '')
 }
 
 export function timestamp(): string {
