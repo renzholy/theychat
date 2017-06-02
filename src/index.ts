aaa1a4f9275e264e3f64d62b48f7885bbd60ac74
RenzHoly <renzholy@gmail.com>
2017-06-02 20:24:15 +0800

    improve print

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,11 @@ import { readFile } from 'fs'
 import { resolve } from 'path'
 
 import { API } from './api'
+import { ContactStore } from './models/ContactStore'
 
 const api = new API()
+
+let store: ContactStore
 
 api.onLogin(() => {
   console.log('login succeed')
@@ -14,10 +17,11 @@ api.onLogin(() => {
 
 api.onContacts((contactStore) => {
   console.log('contacts', contactStore.size())
+  store = contactStore
 })
 
-api.onMessage((msg) => {
-  console.log(msg.toJSON())
+api.onMessage((message) => {
+  console.log(message.type, store.get(message.from).name, store.get(message.to).name, message.speaker ? store.get(message.speaker).name : '', message.text)
 })
 
 api.onError((err) => {
